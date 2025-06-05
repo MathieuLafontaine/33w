76435652907ed9cff372f4bbbcdb5730a5d58757
MathieuLafontaine <mathieu.lafontaine@hotmail.fr>
2025-06-04 21:45:05 -0400

    corrections des images qui ne load pas

--- index.php
+++ index.php
@@ -9,8 +9,8 @@
         </p>
         <a href="#" class="bouton hero__bouton">Lorem</a>
         <picture class="hero__logo">
-            <img src="./image/facebook.svg" alt="logo-facebook" width="32" height="32" />
-            <img src="./image/instagram.svg" alt="logo-instagram" width="32" height="32" />
+            <img src="/wp-content/themes/clubVoyage/image/facebook.svg" alt="logo-facebook" width="32" height="32" />
+            <img src="/wp-content/themes/clubVoyage/image/instagram.svg" alt="logo-instagram" width="32" height="32" />
         </picture>
     </div>
 </section>
@@ -20,35 +20,35 @@
         <h2 class="gallerie__titre">Sautez dans de nouvelles destinations!</h2>
         <section class="gallerie__conteneur">
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-alpes.jpg" alt="alpes" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-alpes.jpg" alt="alpes" class="gallerie__image" />
             </picture>
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-bermudes.jpg" alt="bermudes" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-bermudes.jpg" alt="bermudes" class="gallerie__image" />
             </picture>
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-croisiere.jpg" alt="croisiere" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-croisiere.jpg" alt="croisiere" class="gallerie__image" />
             </picture>
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-fiji.jpg" alt="fiji" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-fiji.jpg" alt="fiji" class="gallerie__image" />
             </picture>
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-irlande.jpg" alt="irlande" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-irlande.jpg" alt="irlande" class="gallerie__image" />
             </picture>
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-japon.jpg" alt="japon" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-japon.jpg" alt="japon" class="gallerie__image" />
             </picture>
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-mexique.jpg" alt="mexique" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-mexique.jpg" alt="mexique" class="gallerie__image" />
             </picture>
             <picture class="gallerie__image__conteneur">
-                <img src="./image/gallerie-rocheuses.jpg" alt="rocheuses" class="gallerie__image" />
+                <img src="/wp-content/themes/clubVoyage/image/gallerie-rocheuses.jpg" alt="rocheuses" class="gallerie__image" />
             </picture>
         </section>
     </section>
     <section class="populaire contenu-principal__conteneur">
         <h2 class="populaire__titre">Sautez dans nos destinations les plus populaires!</h2>
         <section class="populaire__contenu">
-            <picture class="populaire__image__conteneur"><img class="populaire__image" src="./image/populaire.jpg" alt="image-populaire" /></picture>
+            <picture class="populaire__image__conteneur"><img class="populaire__image" src="/wp-content/themes/clubVoyage/image/populaire.jpg" alt="image-populaire" /></picture>
             <p class="populaire__texte">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo consectetur nihil tempora ullam, quaerat
                 ratione quia aperiam incidunt libero autem nemo cum omnis. Quibusdam itaque neque dolorem, officiis maiores
