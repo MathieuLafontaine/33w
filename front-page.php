0d527efe623c2720f29db46bc620be477f649f7b
MathieuLafontaine <mathieu.lafontaine@hotmail.fr>
2025-06-12 13:05:22 -0400

    ajout de la section gallerie selon le modele vue en classe

--- front-page.php
+++ front-page.php
@@ -18,38 +18,8 @@
         </picture>
     </div>
 </section>
-<section class="contenu-principal">
-    <!--Images trouvees sur Unsplash-->
-    <section class="gallerie contenu-principal__conteneur">
-        <h2 class="gallerie__titre">Sautez dans de nouvelles destinations!</h2>
-        <section class="gallerie__conteneur">
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-alpes.jpg" alt="alpes" class="gallerie__image" />
-            </picture>
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-bermudes.jpg" alt="bermudes" class="gallerie__image" />
-            </picture>
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-croisiere.jpg" alt="croisiere" class="gallerie__image" />
-            </picture>
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-fiji.jpg" alt="fiji" class="gallerie__image" />
-            </picture>
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-irlande.jpg" alt="irlande" class="gallerie__image" />
-            </picture>
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-japon.jpg" alt="japon" class="gallerie__image" />
-            </picture>
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-mexique.jpg" alt="mexique" class="gallerie__image" />
-            </picture>
-            <picture class="gallerie__image__conteneur">
-                <img src="/wp-content/themes/clubVoyage/image/gallerie-rocheuses.jpg" alt="rocheuses" class="gallerie__image" />
-            </picture>
-        </section>
-    </section>
-    <!--     <section class="populaire contenu-principal__conteneur">
+</section>
+<!--     <section class="populaire contenu-principal__conteneur">
         <h2 class="populaire__titre">Sautez dans nos destinations les plus populaires!</h2>
         <section class="populaire__contenu">
             <picture class="populaire__image__conteneur"><img class="populaire__image" src="/wp-content/themes/clubVoyage/image/populaire.jpg" alt="image-populaire" /></picture>
@@ -61,39 +31,25 @@
         </section>
     </section> -->
 
-    <section class="populaire">
+<section class="populaire">
+    <div class="conteneur global">
         <?php if (have_posts()) {
             while (have_posts()) {
-                the_post(); ?>
-        <?= the_content();
-            }
-        } ?>
-        <article class="populaire__carte">
-            <?php the_post_thumbnail('thumbnail'); ?>
-            <h2><?php the_title(); ?></h2>
-            <p>
-                <?php
-                /*cette fonction permet d'afficher l'ensemble du contenu du post the content*/
-                $lien = "<a href=" . get_permalink() . ">suite</a>";
-                echo wp_trim_words(get_the_excerpt(), 10, $lien); ?>
-            </p>
-        </article>
-    </section>
-
-    <section class="destinations contenu-principal__conteneur">
-        <h1 class="destinations__titre"><?= the_title(); ?></h1>
-        <?php if (have_posts()) {
-            while (have_posts()) {
-                (the_post()); ?>
-                <h1><?= the_title(); ?></h1>
-        <?= the_content();
-            }
-        } ?>
-        <?php
-        /*Cette fonction affiche l'entierete du <post></post>*/
-        $lien = "<a href=" . get_permalink() . ">Suite</a>";
-        wp_trim_words(get_the_excerpt(), 10, $lien);
+                /* affiche l'image « mise en avant » miniature */
+                the_post();
         ?>
-    </section>
+                <?php
+                if (in_category('galerie')) {
+                    get_template_part("gabarit/galerie");
+                } else {
+                    get_template_part("gabarit/carte");
+                ?>
+
+
+        <?php }
+            }
+        } ?>
+    </div>
+</section>
 </section>
 <?php get_footer(); ?>
